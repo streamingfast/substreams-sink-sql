semiotic-ai/substreams-sink-postgres

use database changes with composite primary keys

db/ops.go
@@ -8,9 +8,11 @@ import (
 
 // Insert a row in the DB, it is assumed the table exists, you can do a
 // check before with HasTable()
-func (l *Loader) Insert(tableName string, primaryKey string, data map[string]string) error {
+func (l *Loader) Insert(tableName string, primaryKey map[string]string, data map[string]string) error {
+	uniqueId := createKeyValuePairs(primaryKey)
+
 	if l.tracer.Enabled() {
-		l.logger.Debug("processing insert operation", zap.String("table_name", tableName), zap.String("primary_key", primaryKey), zap.Int("field_count", len(data)))
+		l.logger.Debug("processing insert operation", zap.String("table_name", tableName), zap.String("primary_key", uniqueId), zap.Int("field_count", len(data)))
 	}
 
 	if _, found := l.entries[tableName]; !found {
@@ -21,27 +23,47 @@ func (l *Loader) Insert(tableName string, primaryKey string, data map[string]str
 		l.entries[tableName] = map[string]*Operation{}
 	}
 
-	if _, found := l.entries[tableName][primaryKey]; found {
+	if _, found := l.entries[tableName][uniqueId]; found {
 		return fmt.Errorf("attempting to insert in table %q a primary key %q, that is already scheduled for insertion, insert should only be called once for a given primary key", tableName, primaryKey)
 	}
 
 	if l.tracer.Enabled() {
-		l.logger.Debug("primary key entry never existed for table, adding insert operation", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+		l.logger.Debug("primary key entry never existed for table, adding insert operation", zap.String("primary_key", uniqueId), zap.String("table_name", tableName))
 	}
 
 	// we need to make sure to add the primary key in the data so that
 	// it gets created
-	data[l.tablePrimaryKeys[tableName]] = primaryKey
-	l.entries[tableName][primaryKey] = l.newInsertOperation(tableName, primaryKey, data)
+	for _, table := range l.tablePrimaryKeys[tableName] {
+		data[table] = primaryKey[table]
+	}
+	l.entries[tableName][uniqueId] = l.newInsertOperation(tableName, primaryKey, data)
 	l.entriesCount++
 	return nil
+}
+func createKeyValuePairs(m map[string]string) string {
+	return fmt.Sprint(m)
+}
+
+func (l *Loader) GetPrimaryKey(tableName string, pk string) (map[string]string, error) {
+	primaryKeyColumns := l.tablePrimaryKeys[tableName]
+	if len(primaryKeyColumns) > 1 {
+		return nil, fmt.Errorf("table %q has composite primary key", tableName)
+	}
+	primaryKey := map[string]string{}
+	for _, column := range primaryKeyColumns {
+		primaryKey[column] = pk
+	}
+
+	return primaryKey, nil
 }
 
 // Update a row in the DB, it is assumed the table exists, you can do a
 // check before with HasTable()
-func (l *Loader) Update(tableName string, primaryKey string, data map[string]string) error {
+func (l *Loader) Update(tableName string, primaryKey map[string]string, data map[string]string) error {
+
+	uniqueId := createKeyValuePairs(primaryKey)
 	if l.tracer.Enabled() {
-		l.logger.Debug("processing update operation", zap.String("table_name", tableName), zap.String("primary_key", primaryKey), zap.Int("field_count", len(data)))
+		l.logger.Debug("processing update operation", zap.String("table_name", tableName), zap.String("primary_key", uniqueId), zap.Int("field_count", len(data)))
 	}
 
 	if _, found := l.entries[tableName]; !found {
@@ -52,35 +74,36 @@ func (l *Loader) Update(tableName string, primaryKey string, data map[string]str
 		l.entries[tableName] = map[string]*Operation{}
 	}
 
-	if op, found := l.entries[tableName][primaryKey]; found {
+	if op, found := l.entries[tableName][uniqueId]; found {
 		if op.opType == OperationTypeDelete {
 			return fmt.Errorf("attempting to update an object with primary key %q, that schedule to be deleted", primaryKey)
 		}
 
 		if l.tracer.Enabled() {
-			l.logger.Debug("primary key entry already exist for table, merging fields together", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+			l.logger.Debug("primary key entry already exist for table, merging fields together", zap.String("primary_key", uniqueId), zap.String("table_name", tableName))
 		}
 
 		op.mergeData(data)
-		l.entries[tableName][primaryKey] = op
+		l.entries[tableName][uniqueId] = op
 		return nil
 	} else {
 		l.entriesCount++
 	}
 
 	if l.tracer.Enabled() {
-		l.logger.Debug("primary key entry never existed for table, adding update operation", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+		l.logger.Debug("primary key entry never existed for table, adding update operation", zap.String("primary_key", uniqueId), zap.String("table_name", tableName))
 	}
 
-	l.entries[tableName][primaryKey] = l.newUpdateOperation(tableName, primaryKey, data)
+	l.entries[tableName][uniqueId] = l.newUpdateOperation(tableName, primaryKey, data)
 	return nil
 }
 
 // Delete a row in the DB, it is assumed the table exists, you can do a
 // check before with HasTable()
-func (l *Loader) Delete(tableName string, primaryKey string) error {
+func (l *Loader) Delete(tableName string, primaryKey map[string]string) error {
+	uniqueId := createKeyValuePairs(primaryKey)
 	if l.tracer.Enabled() {
-		l.logger.Debug("processing delete operation", zap.String("table_name", tableName), zap.String("primary_key", primaryKey))
+		l.logger.Debug("processing delete operation", zap.String("table_name", tableName), zap.String("primary_key", uniqueId))
 	}
 
 	if _, found := l.entries[tableName]; !found {
@@ -91,18 +114,18 @@ func (l *Loader) Delete(tableName string, primaryKey string) error {
 		l.entries[tableName] = map[string]*Operation{}
 	}
 
-	if _, found := l.entries[tableName][primaryKey]; !found {
+	if _, found := l.entries[tableName][uniqueId]; !found {
 		if l.tracer.Enabled() {
-			l.logger.Debug("primary key entry never existed for table", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+			l.logger.Debug("primary key entry never existed for table", zap.String("primary_key", uniqueId), zap.String("table_name", tableName))
 		}
 
 		l.entriesCount++
 	}
 
 	if l.tracer.Enabled() {
-		l.logger.Debug("adding deleting operation", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+		l.logger.Debug("adding deleting operation", zap.String("primary_key", uniqueId), zap.String("table_name", tableName))
 	}
 
-	l.entries[tableName][primaryKey] = l.newDeleteOperation(tableName, primaryKey)
+	l.entries[tableName][uniqueId] = l.newDeleteOperation(tableName, primaryKey)
 	return nil
 }

streamingfast/substreams-sink-sql

db/ops.go
@@ -8,9 +8,11 @@ import (
 
 // Insert a row in the DB, it is assumed the table exists, you can do a
 // check before with HasTable()
-func (l *Loader) Insert(tableName string, primaryKey string, data map[string]string) error {
+func (l *Loader) Insert(tableName string, primaryKey map[string]string, data map[string]string) error {
+	uniqueID := createKeyValuePairs(primaryKey)
+
 	if l.tracer.Enabled() {
-		l.logger.Debug("processing insert operation", zap.String("table_name", tableName), zap.String("primary_key", primaryKey), zap.Int("field_count", len(data)))
+		l.logger.Debug("processing insert operation", zap.String("table_name", tableName), zap.String("primary_key", uniqueID), zap.Int("field_count", len(data)))
 	}
 
 	table, found := l.tables[tableName]
@@ -26,26 +28,48 @@ func (l *Loader) Insert(tableName string, primaryKey string, data map[string]str
 		l.entries[tableName] = map[string]*Operation{}
 	}
 
-	if _, found := l.entries[tableName][primaryKey]; found {
+	if _, found := l.entries[tableName][uniqueID]; found {
 		return fmt.Errorf("attempting to insert in table %q a primary key %q, that is already scheduled for insertion, insert should only be called once for a given primary key", tableName, primaryKey)
 	}
 
 	if l.tracer.Enabled() {
-		l.logger.Debug("primary key entry never existed for table, adding insert operation", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+		l.logger.Debug("primary key entry never existed for table, adding insert operation", zap.String("primary_key", uniqueID), zap.String("table_name", tableName))
 	}
 
-	// we need to make sure to add the primary key in the data so that it gets created
-	data[table.primaryColumn.name] = primaryKey
-	l.entries[tableName][primaryKey] = l.newInsertOperation(table, primaryKey, data)
+	// we need to make sure to add the primary key in the data so that
+	// it gets created
+	for _, primary := range l.tables[tableName].primaryColumn {
+		data[primary.name] = primaryKey[primary.name]
+	}
+	l.entries[tableName][uniqueID] = l.newInsertOperation(table, primaryKey, data)
 	l.entriesCount++
 	return nil
 }
 
+func createKeyValuePairs(m map[string]string) string {
+	return fmt.Sprint(m)
+}
+
+func (l *Loader) GetPrimaryKey(tableName string, pk string) (map[string]string, error) {
+	primaryKeyColumns := l.tables[tableName].primaryColumn
+	if len(primaryKeyColumns) > 1 {
+		return nil, fmt.Errorf("table %q has composite primary key", tableName)
+	}
+	primaryKey := map[string]string{}
+	for _, column := range primaryKeyColumns {
+		primaryKey[column.name] = pk
+	}
+
+	return primaryKey, nil
+}
+
 // Update a row in the DB, it is assumed the table exists, you can do a
 // check before with HasTable()
-func (l *Loader) Update(tableName string, primaryKey string, data map[string]string) error {
+func (l *Loader) Update(tableName string, primaryKey map[string]string, data map[string]string) error {
+
+	uniqueID := createKeyValuePairs(primaryKey)
 	if l.tracer.Enabled() {
-		l.logger.Debug("processing update operation", zap.String("table_name", tableName), zap.String("primary_key", primaryKey), zap.Int("field_count", len(data)))
+		l.logger.Debug("processing update operation", zap.String("table_name", tableName), zap.String("primary_key", uniqueID), zap.Int("field_count", len(data)))
 	}
 
 	table, found := l.tables[tableName]
@@ -61,35 +85,36 @@ func (l *Loader) Update(tableName string, primaryKey string, data map[string]str
 		l.entries[tableName] = map[string]*Operation{}
 	}
 
-	if op, found := l.entries[tableName][primaryKey]; found {
+	if op, found := l.entries[tableName][uniqueID]; found {
 		if op.opType == OperationTypeDelete {
 			return fmt.Errorf("attempting to update an object with primary key %q, that schedule to be deleted", primaryKey)
 		}
 
 		if l.tracer.Enabled() {
-			l.logger.Debug("primary key entry already exist for table, merging fields together", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+			l.logger.Debug("primary key entry already exist for table, merging fields together", zap.String("primary_key", uniqueID), zap.String("table_name", tableName))
 		}
 
 		op.mergeData(data)
-		l.entries[tableName][primaryKey] = op
+		l.entries[tableName][uniqueID] = op
 		return nil
 	} else {
 		l.entriesCount++
 	}
 
 	if l.tracer.Enabled() {
-		l.logger.Debug("primary key entry never existed for table, adding update operation", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+		l.logger.Debug("primary key entry never existed for table, adding update operation", zap.String("primary_key", uniqueID), zap.String("table_name", tableName))
 	}
 
-	l.entries[tableName][primaryKey] = l.newUpdateOperation(table, primaryKey, data)
+	l.entries[tableName][uniqueID] = l.newUpdateOperation(table, primaryKey, data)
 	return nil
 }
 
 // Delete a row in the DB, it is assumed the table exists, you can do a
 // check before with HasTable()
-func (l *Loader) Delete(tableName string, primaryKey string) error {
+func (l *Loader) Delete(tableName string, primaryKey map[string]string) error {
+	uniqueID := createKeyValuePairs(primaryKey)
 	if l.tracer.Enabled() {
-		l.logger.Debug("processing delete operation", zap.String("table_name", tableName), zap.String("primary_key", primaryKey))
+		l.logger.Debug("processing delete operation", zap.String("table_name", tableName), zap.String("primary_key", uniqueID))
 	}
 
 	table, found := l.tables[tableName]
@@ -105,18 +130,18 @@ func (l *Loader) Delete(tableName string, primaryKey string) error {
 		l.entries[tableName] = map[string]*Operation{}
 	}
 
-	if _, found := l.entries[tableName][primaryKey]; !found {
+	if _, found := l.entries[tableName][uniqueID]; !found {
 		if l.tracer.Enabled() {
-			l.logger.Debug("primary key entry never existed for table", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+			l.logger.Debug("primary key entry never existed for table", zap.String("primary_key", uniqueID), zap.String("table_name", tableName))
 		}
 
 		l.entriesCount++
 	}
 
 	if l.tracer.Enabled() {
-		l.logger.Debug("adding deleting operation", zap.String("primary_key", primaryKey), zap.String("table_name", tableName))
+		l.logger.Debug("adding deleting operation", zap.String("primary_key", uniqueID), zap.String("table_name", tableName))
 	}
 
-	l.entries[tableName][primaryKey] = l.newDeleteOperation(table, primaryKey)
+	l.entries[tableName][uniqueID] = l.newDeleteOperation(table, primaryKey)
 	return nil
 }