be809cb65b03ebf3d7f8b718b0525b0e420bee10
colindickson <colin.dickson@gmail.com>
2023-11-27 21:03:02 -0500

    fix typo

--- db/dialect_postgres.go
+++ db/dialect_postgres.go
@@ -243,7 +243,7 @@ func (d postgresDialect) CreateUser(tx Tx, ctx context.Context, l *Loader, usern
             CREATE ROLE %s LOGIN PASSWORD '%s';
             GRANT CONNECT ON DATABASE %s TO %s;
             GRANT USAGE ON SCHEMA public TO %s;
-            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO %s
+            ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT ON TABLES TO %s;
             GRANT SELECT ON ALL TABLES IN SCHEMA public TO %s;
         `, user, pass, db, user, user, user, user)
 	} else {
