a6e9ab289cd1f99bce21babd82d591c422bfdc19
Enol Álvarez <enol826@gmail.com>
2024-07-30 12:07:37 +0200

    Update Protobuf

--- docs/tutorial/src/pb/mod.rs
+++ docs/tutorial/src/pb/mod.rs
@@ -1,3 +1,35 @@
-#[path = "eth.block_meta.v1.rs"]
-#[allow(dead_code)]
-pub mod block_meta;
+// @generated
+pub mod eth_block_meta {
+    // @@protoc_insertion_point(attribute:eth_block_meta.v1)
+    pub mod v1 {
+        include!("eth_block_meta.v1.rs");
+        // @@protoc_insertion_point(eth_block_meta.v1)
+    }
+}
+pub mod sf {
+    pub mod substreams {
+        pub mod sink {
+            pub mod database {
+                // @@protoc_insertion_point(attribute:sf.substreams.sink.database.v1)
+                pub mod v1 {
+                    include!("sf.substreams.sink.database.v1.rs");
+                    // @@protoc_insertion_point(sf.substreams.sink.database.v1)
+                }
+            }
+            pub mod kv {
+                // @@protoc_insertion_point(attribute:sf.substreams.sink.kv.v1)
+                pub mod v1 {
+                    include!("sf.substreams.sink.kv.v1.rs");
+                    // @@protoc_insertion_point(sf.substreams.sink.kv.v1)
+                }
+            }
+            pub mod types {
+                // @@protoc_insertion_point(attribute:sf.substreams.sink.types.v1)
+                pub mod v1 {
+                    include!("sf.substreams.sink.types.v1.rs");
+                    // @@protoc_insertion_point(sf.substreams.sink.types.v1)
+                }
+            }
+        }
+    }
+}
